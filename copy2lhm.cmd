8967b407d8c6db1398566baa877eba48c263e42e
Kyoudai Ken <bloodysword@gmx.net>
2019-11-16 01:10:50 +0100

    Removed a batch script

--- copy2lhm.cmd
+++
@@ -1,3 +0,0 @@
-copy .\web\*.html C:\Portable\OpenHardwareMonitor\Utilities\* /y
-copy .\web\*.js C:\Portable\OpenHardwareMonitor\Utilities\* /y
-copy .\web\*.css C:\Portable\OpenHardwareMonitor\Utilities\* /y
