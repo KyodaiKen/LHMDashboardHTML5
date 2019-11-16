8c4dd0c8053924af95cafafb8e69896d0a346835
Kyoudai Ken <bloodysword@gmx.net>
2019-11-16 01:05:18 +0100

    Init

---
+++ copy2lhm.cmd
@@ -0,0 +1,3 @@
+copy .\web\*.html C:\Portable\OpenHardwareMonitor\Utilities\* /y
+copy .\web\*.js C:\Portable\OpenHardwareMonitor\Utilities\* /y
+copy .\web\*.css C:\Portable\OpenHardwareMonitor\Utilities\* /y
